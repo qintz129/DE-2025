59f1cd6db04e92ccb306b8dc842f17c442818098
Sophie Qin <superpurple273@gmail.com>
2025-01-25 12:43:35 +0000

    models and macros

---
+++ 04-dbt/models/staging/stg_green_tripdata.sql
@@ -0,0 +1,39 @@
+with
+
+    source as (select * from {{ source("staging", "green_tripdata") }}),
+
+    renamed as (
+
+        select
+            {{ dbt_utils.generate_surrogate_key(["vendorid", "tpep_pickup_datetime"]) }}
+            as tripid,
+            vendorid,
+            tpep_pickup_datetime,
+            tpep_dropoff_datetime,
+            store_and_fwd_flag,
+            ratecodeid,
+            passenger_count,
+            trip_distance,
+            fare_amount,
+            extra,
+            mta_tax,
+            tip_amount,
+            tolls_amount,
+            ehail_fee,
+            airport_fee,
+            total_amount,
+            payment_type,
+            {{ get_payment_type_description("payment_type") }} as payment_type_description,
+            distance_between_service,
+            time_between_service,
+            trip_type,
+            improvement_surcharge,
+            pulocationid,
+            dolocationid,
+            data_file_year,
+            data_file_month
+        from source
+    )
+
+select *
+from renamed
